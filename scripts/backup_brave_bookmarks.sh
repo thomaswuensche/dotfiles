8c9c0f23b4afe246f001620ed9f2ec1abb28371f
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2020-10-13 13:58:00 +0200

    🌵

---
+++ scripts/backup_brave_bookmarks.sh
@@ -0,0 +1,8 @@
+current_time=$(date)
+current_date=$(date +"%d_%m_%Y")
+
+bookmarks_location="$HOME/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks"
+backup_location="$HOME/Library/Mobile Documents/com~apple~CloudDocs/backups/bookmark-backups/brave-bookmarks"
+
+echo "\n*** $current_time [backup_brave_bookmarks.sh] ***" >> "$HOME/coding/log.txt"
+command cp -v $bookmarks_location "$backup_location/Bookmarks_$current_date" &>> "$HOME/coding/log.txt"
