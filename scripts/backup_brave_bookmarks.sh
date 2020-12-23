488b6ba0e36097ff678a23a872a0d3184a15d2b2
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2020-12-23 22:11:15 +0100

    bookmarks script & zshrc

--- scripts/backup_brave_bookmarks.sh
+++ scripts/backup_brave_bookmarks.sh
@@ -3,4 +3,4 @@ current_date=$(date +"%d_%m_%Y")
 bookmarks_location="$HOME/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks"
 backup_location="$HOME/Library/Mobile Documents/com~apple~CloudDocs/backups/bookmark-backups/brave-bookmarks"
 
-command cp -v $bookmarks_location "$backup_location/Bookmarks_$current_date" > /dev/null
+command cp -v $bookmarks_location "$backup_location/Bookmarks_$current_date.json" > /dev/null
