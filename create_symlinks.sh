36428b4c5ec0743c0f4d7af39baa69410f7951b4
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2021-01-11 21:52:15 +0100

    edit create_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,25 +1,50 @@
-# files
-command ln -sfhv ~/coding/dotfiles/zsh/thowue.zsh-theme ~/.oh-my-zsh/themes/thowue.zsh-theme
-command ln -sfhv ~/coding/dotfiles/zsh/zshrc ~/.zshrc
-command ln -sfhv ~/coding/dotfiles/vim/vimrc ~/.vimrc
-command ln -sfhv ~/coding/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-command ln -sfhv ~/coding/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-command ln -sfhv ~/coding/dotfiles/git-settings/gitconfig ~/.gitconfig
-command ln -sfhv ~/coding/dotfiles/youtube-dl/config ~/.config/youtube-dl/config
+function delete_prompt() {
+  echo -en "overwrite \033[0;34m$1\033[0m ? "
+  read user_input
+  if [[ $user_input == 'y' ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
+function create_file_symlink() {
+  if [[ -e $2 && ! -h $2 ]]; then
+    delete_prompt $2
+    if [[ $? -eq 0 ]]; then
+      rm $2
+      command ln -sfhv $1 $2
+    fi
+  else
+    command ln -sfhv $1 $2
+  fi
+}
+
+function create_dir_symlink() {
+  if [[ -d $2 && ! -h $2 ]]; then
+    delete_prompt $2
+    if [[ $? -eq 0 ]]; then
+      rm -rf $2
+      command ln -sfhv $1 $2
+    fi
+  else
+    command ln -sfhv $1 $2
+  fi
+}
+
+# files
+create_file_symlink ~/coding/dotfiles/zsh/thowue.zsh-theme ~/.oh-my-zsh/themes/thowue.zsh-theme
+create_file_symlink ~/coding/dotfiles/zsh/zshrc ~/.zshrc
+create_file_symlink ~/coding/dotfiles/vim/vimrc ~/.vimrc
+create_file_symlink ~/coding/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+create_file_symlink ~/coding/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+create_file_symlink ~/coding/dotfiles/git-settings/gitconfig ~/.gitconfig
+create_file_symlink ~/coding/dotfiles/youtube-dl/config ~/.config/youtube-dl/config
+create_file_symlink ~/coding/dotfiles/scripts/format-name.rb /usr/local/bin/format-name
 chmod +x ~/coding/dotfiles/scripts/format-name.rb
-command ln -sfhv ~/coding/dotfiles/scripts/format-name.rb /usr/local/bin/format-name
 
 
 # directories
-function create_dir_symlink() {
-  if [[ -d $2 && ! -h $2 ]]; then
-    echo 'deleted:'
-    rm -rfv $2
-  fi
-  command ln -sfhv $1 $2
-}
-
 create_dir_symlink ~/coding/dotfiles/atom ~/.atom
 create_dir_symlink ~/coding/dotfiles/karabiner ~/.config/karabiner
 create_dir_symlink ~/coding/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
