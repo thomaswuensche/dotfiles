b2610b7fc491c0d34cccbe8795470e5542dae9d6
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2021-02-26 21:46:12 +0100

    config/brave/backup_bookmarks: edit backup location

--- brave/backup_bookmarks.zsh
+++ brave/backup_bookmarks.zsh
@@ -1,6 +1,6 @@
 current_date=$(date +"%d_%m_%Y")
 
 bookmarks_location="$HOME/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks"
-backup_location="$HOME/Library/Mobile Documents/com~apple~CloudDocs/backups/bookmark-backups/brave-bookmarks"
+backup_location="$HOME/Library/Mobile Documents/com~apple~CloudDocs/backups/bookmarks/brave"
 
 command cp -v $bookmarks_location "$backup_location/Bookmarks_$current_date.json" > /dev/null
