a36e674d21969269553e21dc9702f67d4b45a7c9
thomaswuensche <42999314+thomaswuensche@users.noreply.github.com>
2021-02-26 21:49:36 +0100

    feat/scripts/fast_speedtest: add option to log mode (wifi or lan)

--- scripts/fast_speedtest.sh
+++ scripts/fast_speedtest.sh
@@ -1,4 +1,10 @@
 date=$(date)
 result=$(/usr/local/bin/fast --upload)
 
-echo "\n$date\n$result" >> "$LOGS_DIR/speedtests.log"
+if [[ $1 == '--lan' ]]; then
+  mode="LAN"
+else
+  mode="WIFI"
+fi
+
+echo "\n$date\n$mode\n$result" >> "$LOGS_DIR/speedtests.log"
